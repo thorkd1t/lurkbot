70bd9b7b78b4ca340165f5ca9c1626b9d36627d1
thorkd1t <paynet1986@gmail.com>
2017-05-10 00:44:18 +0100

    Delete joinlist.py

--- joinlist.py
+++
@@ -1,3 +0,0 @@
-
-
-joinlist = ["thor10768765", "thebuddha3", "jackfrags", "boxbox", "voyboy", "strippin", "timmac", "scarra", "pokimane", "a_seagull", "doublelift", "dreadztv", "annemunition", "mrbong", "dasmehdi", "pmsproxy", "bikeman", "mrmoonshouse", "forsenlol", "sacriel", "imaqtpie", "giantwaffle", "sodapoppin", "vader", "fairlight_excalibur", "summit1g", "shroud"]
