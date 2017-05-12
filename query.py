c2d02d7250f0aa07f1eba284a85f7ca4faeb6a5e
thorkd1t <paynet1986@gmail.com>
2017-05-12 15:18:32 +0100

    Update query.py

--- query.py
+++ query.py
@@ -3,6 +3,8 @@ import time
 import joinlist
 
 # the tables look like this:
+# db name imaqtpie.db (each channel in joinlist has its own .db)
+# Table name imaqtpie (each db has one table named the same as the channel)
 #___________________________________________________________________
 #  usr  | mesg  | id  |   flags   | channel   |     date/time      |
 #===================================================================
